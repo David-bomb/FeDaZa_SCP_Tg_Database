2296217f5a3177cfb7329c0555a1a36bc81e904e
David <kot.shubin@mail.ru>
2022-04-24 21:39:03 +0300

    Создание отдельного .py файла для языков
    Мы думаем стоит ли переводить все в json

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from language import languages
 
 # Создание бота, импорт токена из отдельного файла .env, установка логирования и соединения с БД
 dotenv_path = join(dirname(__file__), '.env')
@@ -26,35 +27,7 @@ logging.basicConfig(
     level=logging.ERROR
 )
 profile = ''
-languages = {
-    'help': {
-        'RU': 'Здесь вы можете найти полный архив SCP Foundation\n \n \n/browse *номер объекта* - для поиска статьи об объекте \n'
-              '/profile - для просмотра профиля \n/change_language *language* - изменить язык (доступные языки RU, EN)\nПо всем вопросам обращайтесь @vardabomb',
-        'EN': 'Here you can find the full SCP Foundation archive\n \n \n/browse *object number* - to search for an article about the object \n'
-              '/profile - to view the profile \n/change_language *language* - to change the language (available languages RU, EN)\n \n'
-              'Any questions contact @vardabomb'
-    },
-    'Successfully': {
-        'RU': 'Успешно',
-        'EN': 'Successfully'
-    },
-    'edit_photo': {
-        'RU': 'Пришлите новую фотографию:',
-        'EN': 'Submit a new photo:'
-    },
-    'change_photo': {
-        'RU': 'Красивая фотография, но что вы хотите?',
-        'EN': 'Very nice photo, but what do you want?'
-    },
-    'language_error': {
-        'RU': 'Я всё обыскал, нигде не нашёл того, чего вы хотели, или же я допустил ошибку.',
-        'EN': "I searched everywhere, didn't find what you wanted anywhere, or I made a mistake."
-    },
-    'error_in_typing': {
-        'RU': f'Архив не может обработать данный тип информации, используйте команды.',
-        'EN': f'Cannot archive this type of information, tag the command.'
-    }
-}
+
 
 @dp.message_handler(commands=['start'])  # Просто приветствие
 async def send_welcome(msg: types.Message):
