f29b02455e9ed041e14835238c2c290a368a75b1
David <kot.shubin@mail.ru>
2022-04-29 20:54:01 +0300

    восстановление db.py
    случайно удалил недавно

---
+++ db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# Этот файл нужен для переписывания базы данных пользователей в случае нужды
+conn = sqlite3.connect('users.db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS users( 
+   userid INT PRIMARY KEY,
+   username TEXT,
+   name TEXT,
+   level INTEGER,
+   number_of_requests INTEGER,
+   number_of_bugs INTEGER,
+   date_of_registration DATETIME,
+   photo TEXT,
+   nickname TEXT,
+   language TEXT,
+   change_language BOOLEAN)
+""")
+# Создается БД с колонками: id пользователя, "тэг" пользователя в telegram, имя,
+# уровень, кол-во запросов, кол-во вызванных ошибок (чтобы беседовать с проказниками)
